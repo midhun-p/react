f811e266a87b4dda93727dd304bb3b634b94a8d3
Midhun P <midhun.p@qburst.com>
2021-03-23 11:53:32 +0530

    User details added

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+import userLogo from '../assets/img/user.jpg';
+import { Link, useParams } from "react-router-dom"
 
 function UserDetails() {
+    const params = useParams();
+    const userDetails = {
+        name: '',
+        email:'',
+        phone:'',
+        website:'',
+        company:{
+            name:''
+        }
+    }
+    const [user, setuserDetails] = useState(userDetails);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users/' + params["id"])
+            .then(response => response.json())
+            .then(res => {
+                setuserDetails(res);
+            })
+    }, []);
     return (
         <div>
-            usedsdfsd
+            <div className="row">
+                <div className="col-lg-4">
+                    <div className="card card-div">
+                        <img className="card-img-top" src={userLogo} alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">{user.name}</h5>
+                            <p className="card-text">{user.email}</p>
+                            <p className="card-text">{user.phone}</p>
+                            <p className="card-text">{user.website}</p>
+                            <p className="card-text">{user.company.name}</p>
+                            <Link to={'/users/'} className="btn btn-primary">Go back</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
